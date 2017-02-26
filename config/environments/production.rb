5a7abe73fc3b848d1b4aae0ff61b44a8252ace43
Jake Dluhy <dluhy2@illinois.edu>
2017-02-26 15:26:37 -0800

    Add emailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "player-finder_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+  }
+  config.action_mailer.default_url_options = { host: 'warscribe-finder.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
